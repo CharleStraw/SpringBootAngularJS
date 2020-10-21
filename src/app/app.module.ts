44f22c1142d60b3df79fb3579c4fed0cd5332e96
karol <karussloma1@gmail.com>
2020-10-21 07:31:28 +0200

    cOMITING

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,39 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InputComponent } from './Input/input.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { ViewComponent } from './input.input.component';
+import {Router,Routes} from "@angular/router";
+import {FormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
+
+const routes: Routes = [
+{
+path:'',
+component:ViewComponent
+}
+
+
+];
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputComponent,
+    NotFoundComponent,
+    ViewComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule.fo
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
+
+
 export class AppModule { }
