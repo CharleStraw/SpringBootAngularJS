bfe3f6f9f26276c028ab94aaf94cfa39ebf52131
karol <karussloma1@gmail.com>
2020-10-22 13:01:19 +0200

    Changing app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,41 +1,11 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { InputComponent } from './Input/input.component';
-import { NotFoundComponent } from './not-found/not-found.component';
-import { ViewComponent } from './input.input.component';
-import {Router,Routes} from "@angular/router";
-import {FormsModule} from "@angular/forms";
-import {HttpClientModule} from "@angular/common/http";
-
-const routes: Routes = [
-{
-path:'',
-component:ViewComponent
-}
+import { Routes, RouterModule } from '@angular/router';
 
 
-];
-
-
+const routes: Routes = [];
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    InputComponent,
-    NotFoundComponent,
-    ViewComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule.fo
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
-
-
-
-export class AppModule { }
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,39 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { InputComponent } from './Input/input.component';
-import { NotFoundComponent } from './not-found/not-found.component';
-import { ViewComponent } from './input.input.component';
-import {Router,Routes} from "@angular/router";
-import {FormsModule} from "@angular/forms";
-import {HttpClientModule} from "@angular/common/http";
-
-const routes: Routes = [
-{
-path:'',
-component:ViewComponent
-}
-
-
-];
-
-
 
 @NgModule({
   declarations: [
-    AppComponent,
-    InputComponent,
-    NotFoundComponent,
-    ViewComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule.fo
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-
-
-
 export class AppModule { }
